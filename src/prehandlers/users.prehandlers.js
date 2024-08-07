fd8cea4ef2e84822f76e99aa57c630d51b982338
kennedfer <kenned14santos@gmail.com>
2024-08-07 15:11:50 -0300

    feat: Criar um prehandler que valida os dados do body

--- src/prehandlers/users.prehandlers.js
+++ src/prehandlers/users.prehandlers.js
@@ -1,15 +1,41 @@
+import { request } from "express";
 import User from "../models/user.model.js";
 
 class UsersPrehandlers {
   async userNotExists(request, reply, next) {
     const { id } = request.params;
-
     const user = await User.findById(id);
+
     if (!user) {
       return reply.status(404).json({ error: "User not found" });
     }
 
     request.user = user;
+    next();
+  }
+
+  async bodyIsValid({ body }, reply, next) {
+    const { name, email, role, bio, experiences, skills, applications } = body;
+
+    const userData = {
+      name,
+      email,
+      role,
+      bio,
+      experiences,
+      skills,
+      applications
+    };
+
+    const newUser = new User(userData);
+
+    try {
+      await newUser.validate();
+    } catch (err) {
+      return reply.status(400).json({ error: err.message });
+    }
+
+    request.newUser = newUser;
     next();
   }
 }
