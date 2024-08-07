0395d8663121955dc4303994da7b7774eb0acaf4
kennedfer <kenned14santos@gmail.com>
2024-08-07 09:08:26 -0300

    fix: Corrigir o local do 'app.js'

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import sequelize from "./database/config.js";
+
+const app = express();
+
+app.use(express.json());
+
+
+const PORT = 3000;
+
+// Sincronizar o banco de dados
+sequelize
+	.sync()
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`A aplicação está rodando na porta http://localhost:${PORT}`);
+		});
+	})
+	.catch((erro) => {
+		console.error("Não foi possível conectar no banco de dados", erro);
+	});
