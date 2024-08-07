873ec9c63698155129fe3eda9f885b4461439f35
kennedfer <kenned14santos@gmail.com>
2024-08-07 12:40:48 -0300

    feat: Configurar o mongoose/mongo e conecta-lo ao db

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import userRoutes from "./routes/UserRoutes.js";
+import * as dotenv from 'dotenv'
+import mongoose from "mongoose";
 // import sequelize from "./database/config.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -8,6 +12,9 @@ app.use(express.json());
 
 app.use("/users", userRoutes);
 
+mongoose.connect(process.env.MONGODB_URI, {
+  dbName: "projeto-final-m4"
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log("SERVER is running"));
