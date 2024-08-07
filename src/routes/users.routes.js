d0daf435224b3f182c65985ec8200f4f8d2340d9
kennedfer <kenned14santos@gmail.com>
2024-08-07 14:31:05 -0300

    feat: Adicionando o prehandler de 'usuario existe' às rotas que o usarão

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,18 +1,28 @@
 import { Router } from "express"
 import UserController from "../controllers/users.controller.js";
 import 'express-async-errors'
+import usersPrehandlers from "../prehandlers/users.prehandlers.js";
 
 const userRoutes = new Router();
 
 //REST pattern
 userRoutes.get("/", UserController.index);
-userRoutes.get("/:id", UserController.show);
+userRoutes.get("/:id",
+  usersPrehandlers.userNotExists,
+  UserController.show
+);
 userRoutes.post("/", UserController.store);
-userRoutes.put("/:id", UserController.update);
-userRoutes.delete("/:id", UserController.delete);
+userRoutes.put("/:id",
+  usersPrehandlers.userNotExists,
+  UserController.update
+);
+userRoutes.delete("/:id",
+  usersPrehandlers.userNotExists,
+  UserController.delete
+);
 
 //Express error handler
-userRoutes.use(function (err, req, res, next) {
+userRoutes.use(function (err, req, res, _) {
   console.log(err);
   res.status(500).send('Erro interno do servidor');
 });
