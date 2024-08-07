ac4151110c2d2e345e3159d38bbbd38d0ff81f0e
kennedfer <kenned14santos@gmail.com>
2024-08-07 09:16:49 -0300

    fix: Desacoplar a logica de listen do server do sequelize

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
 import express from "express";
-import sequelize from "./database/config.js";
+import userRoutes from "./routes/UserRoutes.js";
+// import sequelize from "./database/config.js";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/users", userRoutes);
+
 
 const PORT = 3000;
+app.listen(PORT, () => console.log("SERVER is running"));
+// // Sincronizar o banco de dados
+// sequelize
+// 	.sync()
+// 	.then(() => {
+// 		app.listen(PORT, () => {
+// 			console.log(`A aplicação está rodando na porta http://localhost:${PORT}`);
+// 		});
+// 	})
+// 	.catch((erro) => {
+// 		console.error("Não foi possível conectar no banco de dados", erro);
+// 	});
 
-// Sincronizar o banco de dados
-sequelize
-	.sync()
-	.then(() => {
-		app.listen(PORT, () => {
-			console.log(`A aplicação está rodando na porta http://localhost:${PORT}`);
-		});
-	})
-	.catch((erro) => {
-		console.error("Não foi possível conectar no banco de dados", erro);
-	});
