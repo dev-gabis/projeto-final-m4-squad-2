b80400e0cc5a3ab7f8edca49dc8ac52151faf14b
kennedfer <kenned14santos@gmail.com>
2024-08-07 09:17:26 -0300

    fix: Comentar a primeira rota (nao queria deletar, ainda)

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -3,7 +3,7 @@ import UserController from "../controllers/UserController.js";
 
 const userRoutes = new Router();
 
-userRoutes.get("/", UserController.index);
+// userRoutes.get("/", UserController.index);
 userRoutes.get("/:id", UserController.show);
 userRoutes.post("/", UserController.store);
 userRoutes.put("/:id", UserController.update);
