8f12839e0f00d0a0b143c32e8c1486929adcd130
isabelle <isabellezsspinheiro@gmail.com>
2024-08-10 18:39:25 -0300

    feat: criando o arquivo de controladores das rotas  de empresa e metodo de uma nova empresa

---
+++ src/controllers/enterprise.controllers.js
@@ -0,0 +1,11 @@
+import Enterprise from "../models/enterprise.models";
+
+class EnterpriseController {
+    async store(request, response){ 
+        const enterpriseData= request.body
+        const newEnterprise= new Enterprise(enterpriseData)
+        await newEnterprise.save()
+        response.json(newEnterprise)
+    }
+
+}
