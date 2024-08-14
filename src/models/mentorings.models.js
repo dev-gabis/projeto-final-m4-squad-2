41c72519f6770b251bd204ecb177f9c21a68ac6c
kennedfer <kenned14santos@gmail.com>
2024-08-14 13:14:24 -0300

    fix: Identar o codigo corretamente

--- src/models/mentorings.models.js
+++ src/models/mentorings.models.js
@@ -1,33 +1,30 @@
-import {model, Schema} from "mongoose"
+import { model, Schema } from "mongoose"
 
 const mentoringsSchema = new Schema({
- 
-    mentor_id: {
-      type: ObjectId,
-      required: true
-    },
-    young_id: {
-      type: ObjectId,
-      required: true,
-    
-    },
-    mentorings_date : {
-        type: Date,
-        required: true
-      },
-    topics: {
-      type: [String],
-      default: []
-    },
-   feedback: {
-      type: String,
-      default: ""
-    },
- 
-  
-  });
-  
-  const Mentorings = model("Mentoring", mentoringsSchema);
-  export default Mentorings;
+  mentor_id: {
+    type: ObjectId,
+    required: true
+  },
+  young_id: {
+    type: ObjectId,
+    required: true,
+
+  },
+  mentorings_date: {
+    type: Date,
+    required: true
+  },
+  topics: {
+    type: [String],
+    default: []
+  },
+  feedback: {
+    type: String,
+    default: ""
+  },
+});
+
+const Mentorings = model("Mentoring", mentoringsSchema);
+export default Mentorings;
 
 
