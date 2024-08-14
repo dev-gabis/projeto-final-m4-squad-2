29c7a7a59509b34a25258b28ef194ef4f594b02e
isabelle <isabellezsspinheiro@gmail.com>
2024-08-14 19:16:12 -0300

    feat: adicionando comandos no crud enterprise

--- src/controllers/enterprise.controllers.js
+++ src/controllers/enterprise.controllers.js
@@ -1,11 +1,36 @@
-import Enterprise from "../models/enterprise.models.js";
+import Enterprise from "../models/enterprise.models";
 
 class EnterpriseController {
-    async store(request, response) {
-        const enterpriseData = request.body
-        const newEnterprise = new Enterprise(enterpriseData)
+    async index (response){
+        const enterprise = await Enterprise.findAll()
+        response.json(enterprise)
+        
+    }
+
+    async show (request, response){ 
+        const {id}= request.params
+        const enterprise = await Enterprise.findById(id)
+        response.json(enterprise)
+    }
+
+    async store (request, response){ 
+        const enterpriseData= request.body
+        const newEnterprise= new Enterprise(enterpriseData)
         await newEnterprise.save()
         response.json(newEnterprise)
+    }
+
+    async update (response){ 
+        const { name, cnpj, adress, email} = body;
+        const data = {name, cnpj, adress, email}
+        const updateEnterprise = await Enterprise.findByIdAndUpdate(data) 
+        response.json(updateEnterprise) 
+    }
+
+    async delete (request, response){ 
+        const {id}= request.params
+        const deleteEnterprise = await Enterprise.findByIdAndDelete(id)
+        response.json(deleteEnterprise) 
     }
 
 }
